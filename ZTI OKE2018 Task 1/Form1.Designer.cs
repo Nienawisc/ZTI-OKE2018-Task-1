2bbf531e82a2a6d0e1803751c00e246da988cf36
Robert Greliński <robert.grelinski@student.put.poznan.pl>
2018-11-27 22:14:45 +0100

    Podstawowa analiza tekstu wraz z łączeniem i wyszukiwaniem danych z dbpedii

---
+++ ZTI OKE2018 Task 1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ZTI_OKE2018_Task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.RichTextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(472, 153);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.Text = "";
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(12, 186);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(472, 363);
+            this.outputTextBox.TabIndex = 1;
+            this.outputTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 555);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 596);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ZTI OKE2018 Task 1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Query.Datasets;
+using VDS.RDF.Writing;
+using VDS.RDF.Writing.Formatting;
+
+namespace ZTI_OKE2018_Task_1
+{
+    public partial class Form1 : Form
+    {
+        class Data
+        {
+            int startIndex, stopIndex;
+            string text;
+            public List<Data> Person = new List<Data>();
+
+            public int StartIndex { get => startIndex; set => startIndex = value; }
+            public int StopIndex { get => stopIndex; set => stopIndex = value; }
+            public string Text { get => text; set => text = value; }
+
+            public void findPersons()
+            {
+                string pattern = "([A-Z][a-z]+ )+";
+                Regex regex = new Regex(pattern);
+                MatchCollection matches = regex.Matches(text);
+                foreach(Match m in matches)
+                {
+                    SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"));
+
+                    //Console.WriteLine(m.ToString());
+                    string find = m.ToString().Substring(0,m.ToString().Length-1);
+
+                    // Ask DBPedia to describe the first thing it finds which is a Person
+                    var query2 = "Select ?name ?person WHERE {?person a <http://dbpedia.org/ontology/Person>. ?person foaf:name ?name. FILTER (?name=\"" + find + "\"@en).} LIMIT 1";
+
+                    //Get the result
+                    var dbpGraph = endpoint.QueryWithResultSet(query2);
+                    if (dbpGraph.Results.Count > 0)
+                    {
+                        Data newPersonData = new Data();
+                        //newPersonData.Text = m.ToString(); //sam tekst
+                        newPersonData.Text = dbpGraph.Results.First()[1].ToString(); //referencja do dbpedii
+                        newPersonData.StartIndex = m.Index;
+                        newPersonData.StopIndex = m.Index + text.Length;
+
+                        Person.Add(newPersonData);
+                    }
+                }
+            }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IGraph g = new Graph();
+
+            //Load using a Filename
+            new TurtleParser().Load(g, "oke17task1Training.xml.ttl");
+
+            //Testing parsed data
+            foreach (var t in g.Triples)
+            {
+               // Console.WriteLine($"Subject: {t.Subject}" + Environment.NewLine + $"Predicate: {t.Predicate}" + Environment.NewLine + $"Object: {t.Object}" + Environment.NewLine);
+            }
+
+            //Get the Query processor
+            ISparqlQueryProcessor processor = new LeviathanQueryProcessor(new InMemoryDataset(g));
+
+            //Use the SparqlQueryParser to give us a SparqlQuery object
+            //Should get a Graph back from a CONSTRUCT query
+            var query = new SparqlQueryParser().ParseFromString("CONSTRUCT { ?s ?p ?o } WHERE {?s ?p ?o}");
+            var results = processor.ProcessQuery(query);
+            if (!(results is IGraph g1)) return;
+
+            //Print out the Results
+            var formatter = new NTriplesFormatter();
+
+            var Triples = g.Triples.Where(q => q.Predicate.ToString().ToLower().Contains("isString".ToLower())).GroupBy(q => q.Object).Select(q => q.First());
+
+            List<Data> dataSet = new List<Data>();
+
+
+            foreach (var t in Triples)
+            {
+                //Console.WriteLine(t.ToString(formatter));
+                string pattern = "char=([0-9]+),([0-9]+)";
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(t.Subject.ToString());
+                if (match.Success)
+                {
+                    Data newData = new Data();
+                    newData.StartIndex = 0;
+                    newData.StopIndex= Int32.Parse(match.Groups[2].Value);
+                    newData.Text = t.Object.ToString().Substring(newData.StartIndex, newData.StopIndex);
+                    dataSet.Add(newData);
+                }
+            }
+            foreach(var data in dataSet)
+            {
+                data.findPersons();
+                foreach(var d in data.Person)
+                {
+                    Console.WriteLine(string.Format("nif:beginIndex:{0},\nnif:endIndex :{1}\nnif:isString:{2}",d.StartIndex,d.StopIndex,d.Text));
+                }
+            }
+
+            Console.WriteLine("Query took " + query.QueryExecutionTime + " milliseconds");
+
+            // ------------------------------------------------------------------------------
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"));
+
+            string find = inputTextBox.Text;
+
+            // Ask DBPedia to describe the first thing it finds which is a Person
+            var query2 = "Select ?name ?person WHERE {?person a <http://dbpedia.org/ontology/Person>. ?person foaf:name ?name. FILTER (?name=\"" + find + "\"@en).} LIMIT 1";
+
+            //Get the result
+            var dbpGraph = endpoint.QueryWithResultSet(query2);
+            if (dbpGraph.Results.Count > 0)
+            { 
+                outputTextBox.Text = "finded in dbpedia:\n";
+                outputTextBox.Text += dbpGraph.Results.First().ToString();
+            }
+            else
+                outputTextBox.Text = "couldnt find";
+
+            RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
+            //rdfxmlwriter.Save(dbpGraph, Console.Out); // view test result details for output.
+            //rdfxmlwriter.Save(dbpGraph, "output.rdf");
+            // ------------------------------------------------------------------------------
+
+            Console.ReadLine();
+        }
+    }
+}
