abc0c17888029feb51fc9a3c8626c4464d106b9c
E34tf <dawid.korach@student.put.poznan.pl>
2019-01-19 18:13:18 +0100

    Update InputData class

--- ZTI OKE2018 Task 1/InputData.cs
+++ ZTI OKE2018 Task 1/InputData.cs
@@ -1,6 +1,6 @@
-﻿using edu.stanford.nlp.ie.crf;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using edu.stanford.nlp.ie.crf;
 
 namespace ZTI_OKE2018_Task_1
 {
@@ -10,11 +10,11 @@ namespace ZTI_OKE2018_Task_1
 	public class InputData
 	{
 		/// <summary>
-		/// Constructor of <see cref="InputData"/>
+		///     Constructor of <see cref="InputData" />
 		/// </summary>
 		/// <param name="text">Examined string</param>
-		/// <param name="address">Which part of input file <see cref="IsString"/> belongs.</param>
-		/// <param name="crfClassifier">NER Classifier of class <see cref="CRFClassifier"/></param>
+		/// <param name="address">Which part of input file <see cref="IsString" /> belongs.</param>
+		/// <param name="crfClassifier">NER Classifier of class <see cref="CRFClassifier" /></param>
 		public InputData(string text, string address, CRFClassifier crfClassifier)
 		{
 			IsString = text ?? throw new ArgumentNullException(nameof(text));
@@ -23,49 +23,49 @@ namespace ZTI_OKE2018_Task_1
 		}
 
 		/// <summary>
-		/// Examined string
+		///     Examined string
 		/// </summary>
 		private string IsString { get; }
 
 		/// <summary>
-		/// Which part of input file <see cref="text"/> belongs.
+		///     Which part of input file <see cref="text" /> belongs.
 		/// </summary>
 		private string Address { get; }
 
 		/// <summary>
-		/// NER Classifier of class <see cref="CRFClassifier"/>
+		///     NER Classifier of class <see cref="CRFClassifier" />
 		/// </summary>
 		private CRFClassifier CrfClassifier { get; }
 
 		/// <summary>
-		/// Creating output data formatted to rdf.
+		///     Creating output data formatted to rdf.
 		/// </summary>
-		/// <param name="nc"><see cref="NerClasses"/> class</param>
-		/// <returns>List of <see cref="NerClasses" categorized elements/></returns>
+		/// <param name="nc"><see cref="NerClasses" /> class</param>
+		/// <returns>List of <see cref="NerClasses" categorized elements /></returns>
 		public IEnumerable<OutputData> GetOntologyEntries(NerClasses nc)
 		{
-			List<OutputData> output = new List<OutputData>();
+			var output = new List<OutputData>();
 
-			string tagOpen = "<" + nc + ">";
-			string tagClose = "</" + nc + ">";
+			var tagOpen = "<" + nc + ">";
+			var tagClose = "</" + nc + ">";
 
-			int i = 0;
-			int j = 0;
-			int k = 0;
-			int count = 0;
+			var i = 0;
+			var j = 0;
+			var k = 0;
+			var count = 0;
 
-			string nerText = Extensions.GetNerText(CrfClassifier, IsString);
+			var nerText = Extensions.GetNerText(CrfClassifier, IsString);
 
-			int occurrences = Extensions.CountStringOccurrences(nerText, tagOpen);
+			var occurrences = Extensions.CountStringOccurrences(nerText, tagOpen);
 
 			while (count < occurrences)
 			{
-				int posA = nerText.IndexOf(tagOpen, i, StringComparison.Ordinal);
-				int posB = nerText.IndexOf(tagClose, j, StringComparison.Ordinal);
+				var posA = nerText.IndexOf(tagOpen, i, StringComparison.Ordinal);
+				var posB = nerText.IndexOf(tagClose, j, StringComparison.Ordinal);
 
-				int adjustedPosA = posA + tagOpen.Length;
+				var adjustedPosA = posA + tagOpen.Length;
 
-				string nazwa = string.Empty;
+				var nazwa = string.Empty;
 
 				if (posA < 0 || posB < 0) continue;
 
@@ -74,7 +74,7 @@ namespace ZTI_OKE2018_Task_1
 
 				nazwa = nerText.Substring(adjustedPosA, posB - adjustedPosA);
 
-				OutputData t = new OutputData(Address, nazwa, nc, IsString.IndexOf(nazwa, k, StringComparison.Ordinal), IsString.IndexOf(nazwa, k, StringComparison.Ordinal) + nazwa.Length - 1);
+				var t = new OutputData(Address, nazwa, nc, IsString.IndexOf(nazwa, k, StringComparison.Ordinal), IsString.IndexOf(nazwa, k, StringComparison.Ordinal) + nazwa.Length - 1);
 				output.Add(t);
 
 				k = IsString.IndexOf(nazwa, k, StringComparison.Ordinal) + nazwa.Length;
